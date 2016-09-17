10cff29fd1ff2d7be90ca6fd85c2096b464ad1cc
kushal <kushal@new-host.home>
2016-09-17 11:51:51 -0400

    fix names for libunwind and libicu52

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -10,7 +10,7 @@ apt-get update -yqq
 apt-get upgrade -yqq
 apt-get install git -yqq
 #install dotnet 
-apt-get install curl libunwind8 gettext libunwind icu -yqq
+apt-get install curl libunwind8 gettext libunwind8 libicu52 -yqq
 curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=827530
 mkdir -p /opt/dotnet && tar zxf dotnet.tar.gz -C /opt/dotnet
 ln -s /opt/dotnet/dotnet /usr/local/bin
