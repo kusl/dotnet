75c39af3644d24da5337caf85f13b88cc24f3e2c
kushal <kushal@new-host.home>
2016-09-17 11:39:35 -0400

    add -yqq to apt-get install

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -9,7 +9,7 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 #install dotnet 
-apt-get install curl libunwind8 gettext
+apt-get install curl libunwind8 gettext -yqq
 curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=827530
 sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
 sudo ln -s /opt/dotnet/dotnet /usr/local/bin
