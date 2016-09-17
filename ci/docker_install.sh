f6e2d35c0754d5d0991c98ca920be12c033b2938
kushal <kushal@new-host.home>
2016-09-17 11:43:32 -0400

    remove sudo

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -11,6 +11,6 @@ apt-get install git -yqq
 #install dotnet 
 apt-get install curl libunwind8 gettext -yqq
 curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=827530
-sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
-sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+mkdir -p /opt/dotnet && tar zxf dotnet.tar.gz -C /opt/dotnet
+ln -s /opt/dotnet/dotnet /usr/local/bin
 
