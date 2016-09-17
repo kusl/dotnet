b0e3bcc1ac0b46d46c6a88cfe73749528e981ce9
kushal <kushal@new-host.home>
2016-09-17 11:09:11 -0400

    add dotnet stuff to ci

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,11 +8,9 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
+#install dotnet 
+sudo apt-get install curl libunwind8 gettext
+curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=827530
+sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+sudo ln -s /opt/dotnet/dotnet /usr/local/bin
 
-# Install phpunit, the tool that we will use for testing
-curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
-
-# Install mysql driver
-# Here you can install any other extension that you need
-docker-php-ext-install pdo_mysql
